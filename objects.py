7c7108786b42821602faff8e5e0a5b4e57dda690
Dariya <dariyachobanova@gmail.com>
2023-04-08 14:48:36 +0200

    Documentation added

--- objects.py
+++ objects.py
@@ -9,32 +9,42 @@ from statsmodels.stats.weightstats import DescrStatsW
 
 PI=np.pi
 """
-on axis phase calculation?? 
-to implement: operator overload '*' for 'Field' and np array
-higher p modes??
+higher p modes?? --> solved: gouy phase times (N+1)
 
-if parax and calculated are very different --> maybe a different profile??
+if parax and calculated are very different --> maybe a different profile?? Modal decompositon
 """
 class Geometry():
+    """
+    Geometry class, contianing parameters that define the geometry of the optical microcavity.
+    """
     def __init__(self,wavelength,L,Rm,pwig,grid_size):
+
         self.wavelength=wavelength
         self.k=2*PI*wavelength
-        self.L=L*wavelength
-        self.Rm=Rm*wavelength
+
+        self.L=L
+        self.Rm=Rm
+
         self.grid_size=grid_size
         self.pwig=pwig
 
-        self.xmax=xmax=np.sqrt(self.Rm*self.L)/2
+        self.xmax=xmax=np.sqrt(self.Rm*self.L)/2 # needs to be updated
         self.x = np.linspace(-xmax,xmax,self.grid_size)
 
     def get_2d_grid(self):
         X,Y=np.meshgrid(self.x,self.x)
         return X,Y
 
-    def mirror_coords(self):        
+    def mirror_coords(self):  
+        """
+        Returns the mirror coordinates zm=L-z(x,y).
+        """      
         return self.L-self.x**2/(2*self.Rm) - self.x**4/(8*self.Rm**3)*(1-self.pwig)
     
     def mirror_planes(self):
+        """
+        Returns 4 planes around the mirror surface, used in the interpolation routine in the 'Mode' class.
+        """
         zmin = self.L-self.Rm+np.sqrt(self.Rm**2-2*self.xmax**2)
         return np.linspace(zmin,self.L,4)
     
@@ -43,77 +53,95 @@ class Field():
     def __init__(self,profile,geometry:Geometry):
         self.geometry=geometry
         self.profile=profile
+
         self.abs=np.abs(self.profile)
         self.intensity=self.abs**2
+
         self.phase=np.angle(self.profile)
+
         self.real=self.profile.real
         self.imag=self.profile.imag
     
+    def __mul__(self, other:np.ndarray):
+        """
+        Operator overload, making a 'Field' object able to
+        multiple with a numpy array. Now the profile is multiplied
+        with the array
+        """
+        return self.profile*other
+    
 class Field1D(Field):
-    def __init__(self,profile,geometry:Geometry):
-        self.geometry=geometry
-        self.profile=profile
-        self.abs=np.abs(self.profile)
-        self.intensity=self.abs**2
-        self.phase=np.angle(self.profile)
-        self.real=self.profile.real
-        self.imag=self.profile.imag
-        self.phase_on_axis=self.phase[self.geometry.grid_size//2]
+    def __init__(self):
+        super().__init__()
+        self.phase_on_axis=self.phase[self.geometry.grid_size//2] #extra paramter for 1D field: the on-axis phase
 
     def power(self):
+        """
+        Returns the intensity weighted power of the field
+        """
         spacing=self.geometry.xmax/self.geometry.grid_size
         weight = abs(self.geometry.x)*2*PI*spacing
         return np.sum(self.intensity*weight)
     
     def phase_moments(self):
+        """
+        Returns the mean and RMS phase of the field
+        """
         weights=self.intensity*abs(self.geometry.x)*PI
         moments = DescrStatsW(self.phase,weights)
         return moments.mean, abs(moments.mean**2 - moments.var)
     
     def phase_shift(self):
-        return Field1D(self.profile*np.exp(-1j*self.phase_moments()[0]),self.geometry)
+        """
+        Shifts the field over the average phase
+        """
+        return Field1D(self*np.exp(-1j*self.phase_moments()[0]),self.geometry)
     
     def phase_plate(self):
-        return Field1D(self.profile*np.exp(1j*(L-self.geometry.k*self.geometry.mirror_coords())),self.geometry)
+        """
+        Phase correction for propagation to the z=L plane
+        """
+        return Field1D(self*np.exp(1j*(self.geometry.L-self.geometry.k*self.geometry.mirror_coords())),self.geometry)
 
 class Field2D(Field):
 
     def power(self):
+        """
+        Returns the intensity weighted power of the field
+        """
         spacing = 2*self.geometry.xmax/self.geometry.grid_size
         return np.sum(self.intensity.flatten()*spacing**2)
 
-    # def phase_moments(self):
-    #     weights=self.intensity*abs(self.geometry.get_2d_grid()[0])*PI
-    #     moments = DescrStatsW(self.phase,weights)
-    #     return moments.mean, np.sqrt(moments.var)
-    
     def cross_section(self):
+        """
+        Returns a 1D field, containing the profile cross-sected at y=0.
+        """
         return Field1D(self.profile[self.geometry.grid_size//2],self.geometry)
     
-    def plot_intensity(self,**kwargs):
-        plt.xlabel(r"x [$\mu m$]");plt.ylabel(r"y [$\mu m$]")
-        plt.imshow(self.intensity,cmap='jet',origin='lower', \
-                   extent=[-self.geometry.xmax,self.geometry.xmax,-self.geometry.xmax,self.geometry.xmax],**kwargs)
-        plt.colorbar()
-
 class Mode():
+    """Class containing paraxial eigenmodes of the cavity (LG basis)."""
+
     def __init__(self,p,l,geometry:Geometry,paraxial):
         self.p=p
         self.l=l
         self.N=2*p+l
-        self.geometry=geometry
-        self.paraxial=paraxial
+        self.geometry=geometry # geometry paramters in which the mode exists
+        self.paraxial=paraxial # paraxial or non-paraxial beam propagation
+
         self.z0 = np.sqrt( self.geometry.L*(self.geometry.Rm-self.geometry.L) )
         self.w0 = np.sqrt(self.z0*self.geometry.wavelength/PI)
 
     def field_profile(self,z=0):
+        """
+        Returns a normalized LG(p,l) mode, as a 2D field profile.
+        """
         X,Y=self.geometry.get_2d_grid()
-        normalization = (-1)**self.p* np.sqrt(factorial(self.p)/(PI*factorial(self.p+self.l)) )
+        normalization = (-1)**self.p* np.sqrt(factorial(self.p)/(PI*factorial(self.p+self.l)) )# mode normalization
 
         wz=self.w0 * np.sqrt( 1 + (z/self.z0)**2 )
-        scaling = np.sqrt(2)/wz
+        scaling = np.sqrt(2)/wz # coordinate scaling
 
-        rho = scaling*np.sqrt(X**2+Y**2)
+        rho = scaling*np.sqrt(X**2+Y**2) 
         theta=np.arctan2(Y,X)
 
         f = (rho)**(self.l)*assoc_laguerre(rho**2,self.p,self.l) * np.exp(-rho**2/2)
@@ -123,9 +151,9 @@ class Mode():
         return np.arctan2(z,self.z0)*(self.N+1)
     
     def angular_spectrum(self,z):
-        kbar=self.geometry.k/(2*PI)
+        kbar=self.geometry.k/(2*PI) 
         psi_fourier = fftpack.fft2(self.field_profile().profile)
-        kbar_x = fftpack.fftfreq(self.geometry.grid_size,2*self.geometry.xmax/self.geometry.grid_size) # domain goed from negative to positive, so 2*maximal value !!
+        kbar_x = fftpack.fftfreq(self.geometry.grid_size,2*self.geometry.xmax/self.geometry.grid_size)
         kxx,kyy=np.meshgrid(kbar_x,kbar_x)
 
         if self.paraxial: # Paraxial propagation, first  order Taylor expansion
@@ -134,95 +162,22 @@ class Mode():
             kbar_z = np.sqrt(kbar**2-kxx**2-kyy**2,dtype=complex)
 
         prop_vector = np.exp(1j*(kbar_z-kbar)*2*PI*z)
+
         return fftpack.ifft2((psi_fourier*prop_vector))
 
     def propagate_mirr(self):
+        """Returns the electric field at the mirror surface"""
         z_planes = self.geometry.mirror_planes()
         psi_z=np.empty((self.geometry.grid_size,len(z_planes)),dtype=complex)
+
         for i,plane in enumerate(z_planes):
-            psi_z[:,i]=self.angular_spectrum(plane)[:,self.geometry.grid_size//2]
+            psi_z[:,i]=self.angular_spectrum(plane)[:,self.geometry.grid_size//2] # cross-section of the propagated field
+
         interp_fcn = interpolate.interp1d(z_planes,psi_z,kind='cubic')
         electric_field = np.diag( interp_fcn(self.geometry.mirror_coords()) ) * np.exp(1j*self.geometry.k*(self.geometry.mirror_coords()))
         return Field1D( electric_field, self.geometry )
     
     def propagate_plane(self):
+        """Returns the electric field at the z=L plane"""
         electric_field = self.angular_spectrum(self.geometry.L)[self.geometry.grid_size//2]*np.exp(1j*self.geometry.k*self.geometry.L)
         return Field1D(electric_field,self.geometry)
-
-#%%
-
-def plot_mirr(e_mirr:Field1D,e_0:Mode,geometry):
-    phase_avg,phase_rms=e_mirr.phase_moments()
-    e_phase_shifted = e_mirr.phase_shift()
-    e_expected = e_0.field_profile(geometry.L)
-    print(e_phase_shifted.phase[geometry.grid_size//2])
-    if e_0.paraxial:
-        paraxial='paraxial'
-    else:
-        paraxial='non-paraxial'
-    fig,axs = plt.subplots(2,1,sharex=True)
-    fig.subplots_adjust(wspace=0.3)
-    plt.xlabel(r"x [$\mu m$]")
-    axs[0].set_title(f"Phase shifted electric field on the mirror surface \n ({paraxial} propagation of {e_0.l}{e_0.p} mode ) \n \
-        Average phase = {phase_avg:.4f}, RMS phase = {phase_rms:.4f}\n \
-        On-axis phase = {e_mirr.phase_on_axis:.4f}, Paraxial phase = {-e_0.get_gouy(geometry.L):.4f}\n \
-        L/(Rm-L)={geometry.L/(geometry.Rm-geometry.L)}, L={geometry.L}, Rm={geometry.Rm}",fontsize=10)
-    
-    axs[0].set_ylabel(r"$E [V \mu m^{-1}]$")
-    axs[0].plot(abs(geometry.x),(e_phase_shifted.real),'.',label=r'Re[$E$]')
-    axs[0].plot(abs(geometry.x),(e_phase_shifted.imag),':',label=r'Im[$E$]')
-    # axs[0].plot(abs(geometry.x),(100*e_phase_shifted.imag),color='orange',label=r'100*Im[$E$]')
-    axs[0].plot(abs(geometry.x),e_phase_shifted.abs,'.',markersize=2,label=r'$|E|$')
-    
-    axs[0].plot(abs(geometry.x),e_expected.cross_section().abs,'k--',label='Expected paraxial result',alpha=0.75)
-    axs[0].legend(fontsize=8)
-
-    axs[1].set_title(f"Phase of phase shifted electric field",fontsize=10)
-    axs[1].set_ylabel(r'Arg $E$ [rad]')
-    axs[1].plot(abs(geometry.x),e_phase_shifted.phase,'r')
-    plt.legend()
-    plt.show()
-
-def plot_plane(e_plane:Field1D,e_0:Mode,geometry):
-    # e_corrected = e_plane.phase_plate()
-    e_phase = e_plane.phase + geometry.k*(geometry.mirror_coords()-L)
-    mask=(geometry.x>0)&(geometry.x<7)
-    if e_0.paraxial:
-        paraxial='paraxial'
-    else:
-        paraxial='non-paraxial'
-    plt.figure()
-    plt.title(f"Corrected phase of electric field at z=L plane \n ({paraxial} propagation)\n"+ \
-              r"$Arg[E]-k\cdot z_m$")
-    plt.xlabel(r'$x \;[\mu m]$')
-    plt.ylabel(r"$Arg \;[rad]$")
-    plt.axhline(-e_0.get_gouy(geometry.L),xmin=0,xmax=1,ls='--',color='r',label=f'Paraxial phase {-e_0.get_gouy(geometry.L):.4f}')
-    plt.axhline(e_plane.phase_on_axis,xmin=0,xmax=1,ls='--',color='k',label=rf'On-axis phase: {e_plane.phase_on_axis:.4f}')
-    plt.plot(geometry.x[mask],e_phase[mask],label=r'$\psi(z=L)\cdot \exp(ikz_m)$')
-    plt.legend()
-    plt.show()
-
-#%%
-wavelength=1
-L=10
-Rm=50
-grid_size=2**8-1
-pwig = 1
-geometry = Geometry(wavelength,L,Rm,pwig,grid_size)
-
-l=0
-p=0
-paraxial=True
-
-Psi_0=Mode(p,l,geometry,paraxial)
-psi_0=Psi_0.field_profile().cross_section()
-psi_expected=Psi_0.field_profile(L).cross_section()
-
-# %%
-
-psi_mirr = Psi_0.propagate_mirr()
-psi_plane=Psi_0.propagate_plane()
-# %%
-plot_mirr(psi_mirr,Psi_0,geometry)
-plot_plane(psi_plane,Psi_0,geometry)
-# %%
---
+++ plot_routines.py
@@ -0,0 +1,69 @@
+from objects import Field,Field1D,Field2D,Geometry,Mode
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_mirr(e_mirr:Field1D,e_0:Mode):
+    
+    geometry=e_mirr.geometry
+    phase_avg,phase_rms=e_mirr.phase_moments()
+    e_phase_shifted = e_mirr.phase_shift()
+    e_expected = e_0.field_profile(geometry.L)
+    print(e_phase_shifted.phase[geometry.grid_size//2])
+    if e_0.paraxial:
+        paraxial='paraxial'
+    else:
+        paraxial='non-paraxial'
+    fig,axs = plt.subplots(2,1,sharex=True)
+    fig.subplots_adjust(wspace=0.3)
+    plt.xlabel(r"x [$\mu m$]")
+    axs[0].set_title(f"Phase shifted electric field on the mirror surface \n ({paraxial} propagation of {e_0.l}{e_0.p} mode ) \n \
+        Average phase = {phase_avg:.4f}, RMS phase = {phase_rms:.4f}\n \
+        On-axis phase = {e_mirr.phase_on_axis:.4f}, Paraxial phase = {-e_0.get_gouy(geometry.L):.4f}\n \
+        L/(Rm-L)={geometry.L/(geometry.Rm-geometry.L)}, L={geometry.L}, Rm={geometry.Rm}",fontsize=10)
+    
+    axs[0].set_ylabel(r"$E [V \mu m^{-1}]$")
+    axs[0].plot(abs(geometry.x),(e_phase_shifted.real),'.',label=r'Re[$E$]')
+    axs[0].plot(abs(geometry.x),(e_phase_shifted.imag),':',label=r'Im[$E$]')
+    # axs[0].plot(abs(geometry.x),(100*e_phase_shifted.imag),color='orange',label=r'100*Im[$E$]')
+    axs[0].plot(abs(geometry.x),e_phase_shifted.abs,'.',markersize=2,label=r'$|E|$')
+    
+    axs[0].plot(abs(geometry.x),e_expected.cross_section().abs,'k--',label='Expected paraxial result',alpha=0.75)
+    axs[0].legend(fontsize=8)
+
+    axs[1].set_title(f"Phase of phase shifted electric field",fontsize=10)
+    axs[1].set_ylabel(r'Arg $E$ [rad]')
+    axs[1].plot(abs(geometry.x),e_phase_shifted.phase,'r')
+    plt.legend()
+    plt.show()
+
+def plot_plane(e_plane:Field1D,e_0:Mode):
+    """
+    Phase plot of the field propagation to z=L.
+    """
+        
+    geometry=e_plane.geometry
+    e_phase = e_plane.phase + geometry.k*(geometry.mirror_coords()-geometry.L)
+    mask=(geometry.x>0)&(geometry.x<7)
+    if e_0.paraxial:
+        paraxial='paraxial'
+    else:
+        paraxial='non-paraxial'
+    plt.figure()
+    plt.title(f"Corrected phase of electric field at z=L plane \n ({paraxial} propagation)\n"+ \
+              r"$Arg[E]-k\cdot z_m$")
+    plt.xlabel(r'$x \;[\mu m]$')
+    plt.ylabel(r"$Arg \;[rad]$")
+    plt.axhline(-e_0.get_gouy(geometry.L),xmin=0,xmax=1,ls='--',color='r',label=f'Paraxial phase {-e_0.get_gouy(geometry.L):.4f}')
+    plt.axhline(e_plane.phase_on_axis,xmin=0,xmax=1,ls='--',color='k',label=rf'On-axis phase: {e_plane.phase_on_axis:.4f}')
+    plt.plot(geometry.x[mask],e_phase[mask],label=r'$\psi(z=L)\cdot \exp(ikz_m)$')
+    plt.legend()
+    plt.show()
+
+def plot_intensity(field:Field2D,**kwagrs):
+    """
+    Intensity plot of a 2D field profile
+    """
+    plt.xlabel(r"x [$\mu m$]");plt.ylabel(r"y [$\mu m$]")
+    plt.imshow(field.intensity,cmap='jet',origin='lower', \
+                   extent=[-field.geometry.xmax,field.geometry.xmax,-field.geometry.xmax,field.geometry.xmax],**kwargs)
+    plt.colorbar()
